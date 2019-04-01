6f02d64f0ee9f6f80bbdc78b59b7a7f43f1509db
wenyide <wenyide@homolo.com>
2019-04-01 17:16:26 +0800

    文件别名

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const path = require('path');
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
+  chainWebpack: (config)=>{
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets',resolve('src/assets'))
+      .set('components',resolve('src/components'))
+      .set('layout',resolve('src/components/layout'))
+  },
   // webpack-dev-server 相关配置
   devServer: {
     // 设置代理
